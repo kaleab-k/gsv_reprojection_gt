f44ca292bc7d3364e3053621aa5954e55bdaab5d
zCompKid <getintouchwithkal@gmail.com>
2019-07-22 21:14:24 +0300

    adding camera (seqNumber) as parameter

--- matlab/panoramic_reprojection/yolo2tbl.m
+++ matlab/panoramic_reprojection/yolo2tbl.m
@@ -1,4 +1,4 @@
-function [results] = yolo2tbl(YOLOResult, GTClassNames, maxSeqNum)
+function [results cam_seq] = yolo2tbl(YOLOResult, GTClassNames, maxSeqNum)
 %YOLO2TBL converts YOLO detection Struct to Table
 %   Detailed explanation goes here
     numImages = length(unique([maxSeqNum]));
@@ -17,6 +17,7 @@ function [results] = yolo2tbl(YOLOResult, GTClassNames, maxSeqNum)
             score_A = [];
             class_C = categorical([GTClassNames]);
             class_A = [];
+            cam_A = [];
             for ii = 1:length(curr_seq_det)
                 if(any(strcmp(GTClassNames, curr_seq_det(ii).class)) && curr_seq_det(ii).width >0 && curr_seq_det(ii).height > 0)
                    bbox = [ curr_seq_det(ii).x curr_seq_det(ii).y curr_seq_det(ii).width curr_seq_det(ii).height ];
@@ -24,12 +25,14 @@ function [results] = yolo2tbl(YOLOResult, GTClassNames, maxSeqNum)
                    class_id = find(class_C==curr_seq_det(ii).class);
                    class_A = [class_A; class_C(class_id)];
                    score_A = [score_A; curr_seq_det(ii).confidence];
+                   cam_A = [cam_A; seq_ii];
                 end
             end
 %             gT{seq_ii+1,class_idx} = gT_A;
             results.Boxes{seq_ii+1} = gT_A;
             results.Scores{seq_ii+1} = score_A;
             results.Labels{seq_ii+1} = class_A;
+            cam_seq{seq_ii+1} = cam_A;
 %         end
     end
 
