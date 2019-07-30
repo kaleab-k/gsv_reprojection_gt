d312caa38ac1bf37654e3b5ef67fecf464157016
zCompKid <getintouchwithkal@gmail.com>
2019-07-30 22:48:22 +0300

    added overlap threshold variable

--- matlab/panoramic_reprojection/targetNMS.m
+++ matlab/panoramic_reprojection/targetNMS.m
@@ -2,14 +2,15 @@ function [GTruthStruct] = targetNMS(GTruthNMS, GTruthStruct, seqNum, GSVImgSrc)
 %TARGETNMS Summary of this function goes here
 %   Detailed explanation goes here
     debug_flag  = 0;
+    overlapThresh = 0.35;
     if (~isempty(GTruthNMS))
         [detectionResults cam_seq] = yolo2tbl(GTruthNMS, unique({GTruthNMS.class}), max([GTruthNMS.seqNumber]));
         Labels_A = [];
         for i = 1:length(detectionResults.Labels)
             Labels_A = [Labels_A; detectionResults.Labels{i,1}];
         end
-        [selectedBboxes, selectedScores, selectedLabels] = selectStrongestBboxMC(cell2mat(detectionResults.Boxes),cell2mat(detectionResults.Scores), Labels_A, cell2mat(cam_seq(:)),  'OverlapThreshold', 0.35);
-    %         [selectedBboxes, selectedScores, selectedLabels] = selectStrongestBboxMulticlass(cell2mat(detectionResults.Boxes),cell2mat(detectionResults.Scores), Labels_A,  'OverlapThreshold', 0.3);
+        [selectedBboxes, selectedScores, selectedLabels] = selectStrongestBboxMC(cell2mat(detectionResults.Boxes),cell2mat(detectionResults.Scores), Labels_A, cell2mat(cam_seq(:)),  'OverlapThreshold', overlapThresh);
+    %         [selectedBboxes, selectedScores, selectedLabels] = selectStrongestBboxMulticlass(cell2mat(detectionResults.Boxes),cell2mat(detectionResults.Scores), Labels_A,  'OverlapThreshold', overlapThresh);
         for i = 1:size(selectedBboxes,1)
             xmin_nms = selectedBboxes(i,1);
             ymin_nms = selectedBboxes(i,2);
@@ -28,7 +29,7 @@ function [GTruthStruct] = targetNMS(GTruthNMS, GTruthStruct, seqNum, GSVImgSrc)
             GTruthStruct(end).confidence = score;
             GTruthStruct(end).dataSource = GSVImgSrc;
             if debug_flag == 1
-    %                 figure(5), rectangle('Position', [xmin_nms+0.2, ymin_nms+0.2, width_nms, height_nms],'EdgeColor','g', 'LineWidth', 2);
+                figure(5), rectangle('Position', [xmin_nms+0.2, ymin_nms+0.2, width_nms, height_nms],'EdgeColor','g', 'LineWidth', 2);
                 figure(6), rectangle('Position', [xmin_nms+0.2, ymin_nms+0.2, width_nms, height_nms],'EdgeColor','g', 'LineWidth', 2);
             end
         end
